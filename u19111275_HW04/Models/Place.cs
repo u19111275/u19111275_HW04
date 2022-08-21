bb67e5eb683f6f040bf036c1df8148805b905592
u19111275 <u19111275@tuks.co.za>
2022-08-21 23:28:19 +0200

    places

---
+++ u19111275_HW04/Models/Place.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u19111275_HW04.Models
+{
+    public class Place
+    {
+        public string PlaceName { get; set; }
+        public string contact { get; set; }
+
+    }
+}
